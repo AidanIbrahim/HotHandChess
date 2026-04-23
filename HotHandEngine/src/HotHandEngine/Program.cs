d0340feca5fb5cf3a5020e01a81b078837b72a06
AidanIbrahim <aidan.ibrahim@outlook.com>
2026-04-23 15:24:33 -0400

    Initial Commit

---
+++ HotHandEngine/src/HotHandEngine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HotHandEngine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, HotHand Systems!");
+        }
+    }
+}
