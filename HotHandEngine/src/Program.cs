e9df6469261d03ff7b9c087c3a7e96bbe01bc608
AidanIbrahim <aidan.ibrahim@outlook.com>
2026-04-29 07:07:28 -0400

    implemented command reading loop

--- HotHandEngine/src/Program.cs
+++ HotHandEngine/src/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace HotHandEngine
+﻿using Chess.Core;
+using System;
+using System;
+namespace HotHandEngine
+ 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, HotHand Systems!");
+
+            UCIIntepreter engine = new();
+            string command = String.Empty;
+            while (command != "quit")
+            {
+                command = Console.ReadLine();
+                engine.readCommand(command);
+            }
         }
     }
 }
